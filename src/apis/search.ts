0caa83d883ec6ac49c29043c40ba68cb3a16cb7e
Maximo De Leon <maximodleon@gmail.com>
2025-03-10 07:15:00 -0400

    Add constant to hold the base url for open library

--- src/apis/search.ts
+++ src/apis/search.ts
@@ -1,10 +1,11 @@
-import { type SearchParams, type SearchResponse } from '../types/search-api.ts'
+import { type SearchParams, type SearchResponse } from '../types/search-api.ts';
+import { BASE_URL } from '../constants/index.ts';
 
 export async function getTitle(params: SearchParams): Promise<SearchResponse> {
   const urlSearchParams = new URLSearchParams();
   urlSearchParams.append('q', params.q);
 
-  const url = new URL(`https://openlibrary.org/search.json?${urlSearchParams}`);
+  const url = new URL(`${BASE_URL}/search.json?${urlSearchParams}`);
 
   const options: RequestInit = {
     method: "GET",
