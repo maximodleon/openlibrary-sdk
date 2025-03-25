ef5e53b335b0895b92c3781bcb7015fb90c5e265
Maximo De Leon <maximodleon@gmail.com>
2025-03-25 07:04:31 -0400

    improve example in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import * as search from './src/index.ts';
-import {  getBookCoverFromId, getAuthorPhoto } from './src/utils/index.ts';
+import {  getBookCoverUrlFromId, getAuthorPhotoUrlFromId } from './src/utils/index.ts';
 
-const book = getBookCoverFromId('ISBN', '0385472579', 'M');
-const author = getAuthorPhoto('olid', 'OL229501A', 'M');
-console.log(`${book} ${author}`);
+const result = await search.searchBook({ q: 'don quijote'});
+const first = result.docs[0];
+
+const book = getBookCoverUrlFromId('id', `${first.cover_i}`,  'M');
+const authorImages = first.author_key.map(key => getAuthorPhotoUrlFromId('olid', key, 'M'));
+console.log(`${JSON.stringify(first)}`);
+console.log(`${book}`);
+console.log(`${authorImages}`);
